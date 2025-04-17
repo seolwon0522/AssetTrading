7e645187d037010ffefdae48677f821ec5be1e4f
heysep <limyosep0274@gmail.com>
2025-04-17 20:49:51 +0900

    dto 제작 챗룸 제외

--- src/main/java/com/example/AssetTrading/Dto/ChatRoomRequestDto.java
+++ src/main/java/com/example/AssetTrading/Dto/ChatRoomRequestDto.java
@@ -1,4 +1,14 @@
 package com.example.AssetTrading.Dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ChatRoomRequestDto {
+    private Integer transaction_idx;     // 거래 ID
+    private String buyer_user_id;        // 구매자 ID
+    private String seller_user_id;       // 판매자 ID
 }
