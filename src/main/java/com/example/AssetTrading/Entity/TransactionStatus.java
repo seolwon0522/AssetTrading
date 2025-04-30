2ba2188a909cac26441ff44764f9ba4e3e99a18d
taegyunmon <119221972+taegyunmon@users.noreply.github.com>
2025-04-30 09:05:48 +0900

    Update TransactionStatus.java

--- src/main/java/com/example/AssetTrading/Entity/TransactionStatus.java
+++ src/main/java/com/example/AssetTrading/Entity/TransactionStatus.java
@@ -1,6 +1,7 @@
 package com.example.AssetTrading.Entity;
 
 public enum TransactionStatus {
+    WAITTING, // 대기 상태 ( 상품을 올렸지만 아무 거래가 안 걸린 상태 ) 
     REQUESTED, // 거래 요청
     PROCESSING, // 거래 중
     COMPLETED, // 거래 성공
